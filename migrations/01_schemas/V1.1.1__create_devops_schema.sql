247d037cc230d23bf945bd200f9442c1d5a35ab6
onlyinfra <team.only.infra@gmail.com>
2025-10-09 14:30:31 -0500

    trigger flow

--- migrations/01_schemas/V1.1.1__create_devops_schema.sql
+++ migrations/01_schemas/V1.1.1__create_devops_schema.sql
@@ -1,6 +1,6 @@
 -- Creates the DEVOPS schema in the environment-specific BRONZE_DEVOPS database
 -- Production: BRONZE_DEVOPS.DEVOPS
--- Dev:        BRONZE_DEVOPS_DEV.DEVOPS
+-- Dev:        BRONZE_DEVOPS_DEV.DEVOPS 
 
 CREATE SCHEMA IF NOT EXISTS IDENTIFIER('BRONZE_DEVOPS{{ environment_suffix }}.DEVOPS');
 
