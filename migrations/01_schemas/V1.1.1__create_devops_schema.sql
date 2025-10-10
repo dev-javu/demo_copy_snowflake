0d0c4bba5babdb62369f10b2f87ad6cc5235392e
onlyinfra <team.only.infra@gmail.com>
2025-10-10 12:53:24 -0500

    clean up

--- migrations/01_schemas/V1.1.1__create_devops_schema.sql
+++
@@ -1,6 +0,0 @@
--- Creates the DEVOPS schema in the environment-specific BRONZE_DEVOPS database
--- Production: BRONZE_DEVOPS.DEVOPS
--- Dev:        BRONZE_DEVOPS_DEV.DEVOPS 
-
-CREATE SCHEMA IF NOT EXISTS IDENTIFIER('BRONZE_DEVOPS{{ environment_suffix }}.DEVOPS');
-
