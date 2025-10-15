e8c1ec03212a0ba59a30ba5ec4c1181c42faf425
onlyinfra <team.only.infra@gmail.com>
2025-10-15 12:56:41 -0500

    add jinja

--- migrations/01_schemas/create_schema.sql
+++ migrations/01_schemas/create_schema.sql
@@ -1,5 +1,2 @@
--- Script to create the "DATA" schema in the database
--- Uses fixed base DB 'BRONZE_DEVOPS' and the suffix as provided by the CLI
-
--- Create DATA schema in target DB without changing session database
-CREATE SCHEMA IF NOT EXISTS {{ 'BRONZE_DEVOPS' ~ (variables.ENV_SUFFIX | default('')) }}.DATA;
+-- Create DATA schema in BRONZE_DEVOPS with raw suffix
+CREATE SCHEMA IF NOT EXISTS BRONZE_DEVOPS{{ variables.ENV_SUFFIX }}.DATA;
