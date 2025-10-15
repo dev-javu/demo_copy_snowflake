b86fb41aa5aa31a648c9cd4deb36674834335c0e
onlyinfra <team.only.infra@gmail.com>
2025-10-15 12:45:35 -0500

    updating sql

--- migrations/01_schemas/create_schema.sql
+++ migrations/01_schemas/create_schema.sql
@@ -1,5 +1,5 @@
 -- Script to create the "DATA" schema in the database
 -- Uses fixed base DB 'BRONZE_DEVOPS' and the suffix as provided by the CLI
 
--- Create DATA schema in target DB without changing session database 
+-- Create DATA schema in target DB without changing session database
 CREATE SCHEMA IF NOT EXISTS {{ 'BRONZE_DEVOPS' ~ (variables.ENV_SUFFIX | default('')) }}.DATA;
